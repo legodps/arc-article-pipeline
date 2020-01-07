9b3bd066a50a14a55836f3a88136d2138a712d1a
David Sander <legodps@gmail.com>
2020-01-07 18:14:21 -0500

    Add in file loading for JSONL article files as specified by professor given files
    This commit includes handling for the loading in of JSONL files containing JSON formatted articles.
    I have put in some logic to make the usage a little easier, including the appending of the `.jsonl`
    extension if not present, and the importing of a series of files rather than just a singular `.jsonl`
    file. This may need to be amended if I wish to import the articles in separate objects (making an
    list of lists) but I will talk with the professor about that to see what they think. With the set
    of articles I have, memory does not appear to be a major hurdle to overcome, as I could import all
    2112 articles and keep them in memory. I continue to expand coverage, and will have the Elasticsearch
    functions added to the list of unit tested items once I've gotten a better handle on how that should
    be laid out.

--- pipeline/__main__.py
+++ pipeline/__main__.py
@@ -2,14 +2,19 @@ import argparse
 from pipeline import article_archiver, load_files
 
 parser = argparse.ArgumentParser(description='Store articles into indices to mirror ARC-Solver setup')
+parser.add_argument('filepath', help='a filepath to a singular article file or a directory of files')
 parser.add_argument('-c','--config_file', default='pipelineConfig.yaml', help='A yaml config file to import settings from, defaults to pipelineConfig.yaml')
 
 
-def process_articles(config_file):
+def process_articles(filepath, config_file):
+    """
+
+    """
     properties = load_files.load_config(config_file)
-    print(properties)
+    articles = load_files.read_jsonl_articles(filepath)
+    print(len(articles))
 
 
 args = parser.parse_args()
 
-process_articles(args.config_file)
+process_articles(args.filepath, args.config_file)
--- pipeline/load_files.py
+++ pipeline/load_files.py
@@ -1,3 +1,5 @@
+import json
+import os
 import yaml
 
 
@@ -15,3 +17,62 @@ def load_config(config_path):
         config = yaml.load(config_file, Loader=yaml.FullLoader)
 
     return config
+
+
+def process_article_line(article_line):
+    """ Takes in a JSON formatted line from an article JSONL file and extracts the article of text
+
+        Args:
+            article_line (str): a line from a JSONL file that contains the correct formatting
+
+        Returns:
+            str: the extracted article put together
+    """
+    article_object = json.loads(article_line)
+    article_text = ''
+    for paragraph in article_object['paragraphs']:
+        for text_piece in paragraph['para_body']:
+            article_text += text_piece['text']
+    return article_text
+
+
+def process_article(filepath):
+    """ Opens a JSONL file and returns all articles from it
+
+        Args:
+            filepath (str): the path to the file
+
+        Returns:
+            list: all articles in the file
+    """
+    articles = []
+    # appends the JSONL extension if it does not exist in the path to the file
+    filename = filepath if '.jsonl' in filepath else f'{filepath}.jsonl'
+    with open(filename) as article_file:
+        article_line = article_file.readline()
+        while article_line:
+            articles.append(process_article_line(article_line))
+            article_line = article_file.readline()
+    return articles
+
+
+def read_jsonl_articles(filepath):
+    """ Opens the file(s) at a given filepath, extracts the articles, and returns them
+
+        Args:
+            filepath (str): a path to a given file or directory containing JSONL files
+
+        Returns:
+            list: all articles at the filepath
+    """
+    all_articles = []
+    try:
+        # try to open filepath as a singular file
+        all_articles = process_article(filepath)
+    except (IsADirectoryError, FileNotFoundError):
+        # if it errors, try to open it as a directory containing JSONL files
+        for filename in os.listdir(filepath):
+            if '.jsonl' in filename:
+                all_articles += process_article(f'{filepath}/{filename}')
+
+    return all_articles
